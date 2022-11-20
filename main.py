56496d742aab97d7b04e93721973464b825d9269
Blue0210 <bichvan.truong.keo@gmai.com>
2022-11-20 08:07:33 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.ttk import *
+
+#Create the main window of application
+root = Tk()
+
+root.title("first program")
+
+label = Label(root,text = "Hello World").pack()
+
+root.mainloop()
+
